9c3600398f91fc325b7501f1526a2527c4b13851
W-Johnson <william.johnson@epita.fr>
2022-02-20 17:19:48 +0100

    feat: s3 amazon env heroku

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'f538e9f8b14eb68e3da4f922b2190802'),
+    secret: env("ADMIN_JWT_SECRET", "f538e9f8b14eb68e3da4f922b2190802"),
   },
 });
